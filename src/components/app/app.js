72ce722edcdbd091ee1121c5a5df7c35f6b89db3
Igor Podgorniy <ipod-92@mail.ru>
2021-11-23 19:09:42 +0300

    Вынесение блока с текстом в отдельный компонент.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Modal from "../modal";
 import Motivation from "../motivation";
+import Text from "../text";
 
 import "./app.css";
 
@@ -18,25 +19,7 @@ export default class App extends React.Component {
         </header>
         <Motivation />
         <article>
-          <div className="text container-item">
-            <span className="passed-text">C</span>
-            <span className="current-text">o</span>
-            <span className="wrong">n</span>sectetur dolore consequat
-            landjaeger. Nulla brisket ground round, in sint shankle veniam
-            meatball aute cillum kielbasa t-bone filet mignon. Eu turkey ribeye,
-            nulla sausage commodo quis chicken strip steak lorem ham. Andouille
-            capicola et turkey veniam in pork loin, nostrud sed pork belly
-            consectetur rump pork chop. Swine bacon nulla, frankfurter
-            incididunt tenderloin sunt duis ut adipisicing. Proident enim ad
-            buffalo sirloin voluptate id ut ullamco in exercitation. Tail magna
-            pork loin sirloin. Nisi prosciutto turkey cupidatat, commodo
-            tenderloin spare ribs elit exercitation kielbasa laborum id
-            consequat buffalo labore. Dolore culpa chicken, in jerky irure beef
-            ribs jowl et pork chop tail pastrami turkey chuck tempor. Corned
-            beef swine adipisicing biltong. Dolore laboris pancetta, kevin
-            exercitation dolor minim culpa quis beef ribs chislic. Chuck est
-            adipisicing tongue.
-          </div>
+          <Text />
         </article>
         <footer className="stat container-item">
           <div className="stat-symbol">
---
+++ src/components/text/text.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import "./text.css";
+
+const Text = () => {
+  return (
+    <div className="text container-item">
+      <span className="passed-text">C</span>
+      <span className="current-text">o</span>
+      <span className="wrong">n</span>sectetur dolore consequat landjaeger.
+      Nulla brisket ground round, in sint shankle veniam meatball aute cillum
+      kielbasa t-bone filet mignon. Eu turkey ribeye, nulla sausage commodo quis
+      chicken strip steak lorem ham. Andouille capicola et turkey veniam in pork
+      loin, nostrud sed pork belly consectetur rump pork chop. Swine bacon
+      nulla, frankfurter incididunt tenderloin sunt duis ut adipisicing.
+      Proident enim ad buffalo sirloin voluptate id ut ullamco in exercitation.
+      Tail magna pork loin sirloin. Nisi prosciutto turkey cupidatat, commodo
+      tenderloin spare ribs elit exercitation kielbasa laborum id consequat
+      buffalo labore. Dolore culpa chicken, in jerky irure beef ribs jowl et
+      pork chop tail pastrami turkey chuck tempor. Corned beef swine adipisicing
+      biltong. Dolore laboris pancetta, kevin exercitation dolor minim culpa
+      quis beef ribs chislic. Chuck est adipisicing tongue.
+    </div>
+  );
+};
+
+export default Text;
